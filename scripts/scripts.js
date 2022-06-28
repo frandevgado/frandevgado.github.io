08bacf078a7800252aa9c717fdf4183df84d8274
Franco Delgado <francodevgado@gmail.com>
2022-06-27 23:43:04 -0300

    Desafio complementario: incorporando arrays

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,15 +1,46 @@
-let user = prompt('Ingrese usuario')
-let password = prompt('Ingrese contraseña')
+// → No hice comprobaciones de los datos, obvie que las respuestas fueran tecnicamente correctas e.e
+// → Agrego en caso de que me indiques que lo haga
 
-while (user != 'Gustavo' || password != 212739){
-user = prompt('Ingrese usuario')
-password = prompt('Ingrese contraseña')
+let firstName = prompt('Ingrese su nombre')
+
+alert(`Bienvenido ${firstName}!`)
+
+class Destinos{
+    constructor(nombre, fecha, monto){
+        this.nombre = nombre
+        this.fecha = fecha
+        this.monto = monto
+    }
 }
 
-let number = parseInt(prompt(`Bienvenido ${user}! Ingrese un número del 1 al 10 por favor`))
 
-for(i = 1; i <= number; i++){
-    console.log(number * i)
+const arrayDestinos = []
+
+/*Acá utilizo WHILE para pedir 3 destinos y colocar c/u dentro de un
+array de objetos (arrayDestinos.push) colocando el límite en 3 con la propiedad length*/
+
+while(arrayDestinos.length < 3){
+    if(arrayDestinos.length === 0){
+    let nombre = prompt('Ingrese nombre del primer país que quisiera visitar')
+    let fecha = prompt('¿En que año?')
+    let monto = prompt('¿Cual monto es su límite a utilizar para el mismo?')
+    arrayDestinos.push(new Destinos(nombre, fecha, monto))
+    alert('Aún quedan 2 destinos')
+    }
+    else if(arrayDestinos.length === 1){
+        let nombre = prompt('¿Cual es el segundo país al que le gustaría viajar?')
+        let fecha = prompt('¿En que año?')
+        let monto = prompt('¿Cual monto es su límite a utilizar para el mismo?')
+        arrayDestinos.push(new Destinos(nombre, fecha, monto))
+        alert('¡Perfecto! Falta el último destino')
+    }else if(arrayDestinos.length === 2){
+            let nombre = prompt('¿A qué país le gustaría viajar?')
+            let fecha = prompt('¿En que año?')
+            let monto = prompt('¿Cual monto es su límite a utilizar para el mismo?')
+            arrayDestinos.push(new Destinos(nombre, fecha, monto))
+            alert('Gracias, enseguida le entregaremos las disponibilides')
+            }
+    
 }
 
-
+console.log(arrayDestinos)
